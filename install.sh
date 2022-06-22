98eb0a1bfb02a5b6a3c06c6ed8af948a780f3007
patatakartata <106821289+patatakartata@users.noreply.github.com>
2022-06-22 15:09:07 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 cd ~/cloudshell_open/Cloud_Shell_db1000n; cp install.sh; cd ~/; 
 source <(curl https://raw.githubusercontent.com/Arriven/db1000n/main/install.sh);
-echo -e "\033[1;44m  '"Death by 1000 needles"' готовий працювати. \033[0m"; 
-echo -e "\033[1;43m    Розпочинаємо робити нашу добру справу!   \033[0m"; 
-sleep 4; tput setaf 4; ./db1000n -scale 0.01
+echo -e "\033[1;44m  '"Death by 1000 needles"' вітає тебе друже! \033[0m"; 
+echo -e "\033[1;43m      Починаємо робити нашу добру справу!   \033[0m"; 
+sleep 4; tput setaf 3; ./db1000n
