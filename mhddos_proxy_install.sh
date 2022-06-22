7011a8e66d3e31a9024631aa5f1d988903291e19
patatakartata <106821289+patatakartata@users.noreply.github.com>
2022-06-22 23:47:47 +0300

    Update mhddos_proxy_install.sh

--- mhddos_proxy_install.sh
+++ mhddos_proxy_install.sh
@@ -2,4 +2,7 @@ cd ~/ && rm -rf mhddos_proxy &&
 git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git &&
 cd mhddos_proxy && python3 -m pip install -r requirements.txt &&
 pip install colorama tabulate aiohttp &&
-./runner.sh python3 -t 300 --itarmy --vpn
+echo -e "\033[1;44m     Україна вітає тебе друже!     \033[0m"; 
+echo -e "\033[1;43m    Давай зробимо добру справу!    \033[0m"; 
+tput setaf 6; tput bold; echo "Ініціюю  запуск  програми  MHDDoS_proxy";
+sleep 6; ./runner.sh python3 -t 300 --itarmy --vpn
