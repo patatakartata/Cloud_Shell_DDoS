707a693bd0f03a46fd9466dc1a1e62b2baa7a6cc
patatakartata <106821289+patatakartata@users.noreply.github.com>
2022-06-25 07:09:10 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -36,7 +36,7 @@ fi
 tar xvf "${ARCHIVE}"
 
 tput setaf 3; tput bold; echo "Death by 1000 needles successfully installed."
-echo -e "\033[1;44m     Україна вітає тебе друже!     \033[0m"; 
-echo -e "\033[1;43m    Давай зробимо добру справу!    \033[0m"; 
-tput setaf 6; tput bold; echo "Ініціюю  запуск  програми  db1000n";
+echo -e "\033[1;44m     Україна вітає тебе друже!     \033[0m" 
+echo -e "\033[1;43m    Давай зробимо добру справу!    \033[0m" 
+tput setaf 6; tput bold; echo "Ініціюю  запуск  програми  db1000n" 
 sleep 6; ./db1000n --scale 0.5
