350bd9ca94dce14c963f50e797e8586797827926
patatakartata <106821289+patatakartata@users.noreply.github.com>
2022-06-29 08:21:04 +0300

    Update Tor_install.sh

--- Tor/Tor_install.sh
+++ Tor/Tor_install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#source <(curl https://raw.githubusercontent.com/patatakartata/Cloud_Shell_DDoS/main/Tor/Tor_install.sh)
+#wget https://raw.githubusercontent.com/patatakartata/Cloud_Shell_DDoS/main/Tor/Tor_install.sh
 
 sudo apt install -y tor tor-geoipdb proxychains torsocks geoip-bin
 sudo echo "ExcludeExitNodes {ua} StrictNodes 1" >> /etc/tor/torrc
-sudo echo "ExitNodes {ru},{by},{kz},{am},{kg},{tj},{az} StrictNodes 0" >> /etc/tor/torrc
 sudo service tor start
 sudo service tor restart
